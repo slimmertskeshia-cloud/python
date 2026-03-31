ca2770fb1dccb312bb294faf5c481e7347c0bead
slimmertskeshia-cloud <slimmertskeshia@gmail.com>
2026-03-31 20:13:21 +0200

    Add files via upload

---
+++ Chapter_5/ownwork/comparison.py
@@ -0,0 +1,19 @@
+#1. Equality
+a = 1500
+b = 2000
+print (a ==b)
+
+#2. Inequality
+a = 1500
+b = 2000
+print (a != b)
+
+
+#3. and operator
+print (a > 1000 and b > 1000)
+
+#4 or operator
+print (a > 1000 or b > 3000)
+
+#5 and / or operator
+print (a > 1000 and b > 3000 or a < 2000 and b < 3000)
